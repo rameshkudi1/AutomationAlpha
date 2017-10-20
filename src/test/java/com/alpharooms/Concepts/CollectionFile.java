46f20c695d70a054d223cb6aec13a28427ae9f8a
Ramesh Kudiala <rameshkudikala1>
2017-10-20 16:50:27 +0530

    Test Commit

---
+++ src/test/java/com/alpharooms/Concepts/CollectionFile.java
@@ -0,0 +1,12 @@
+package com.alpharooms.Concepts;
+
+import org.testng.annotations.Test;
+
+public class CollectionFile {
+	
+	@Test
+	public void test(){
+		System.out.println("This is the newly added file");
+	}
+
+}
